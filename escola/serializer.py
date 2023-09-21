dc6a3b6e7e28bb3d589c5263f396365ac2f45439
hectorsalg <hectorsalg@ufpi.edu.br>
2023-09-21 16:42:15 -0300

    Permissões, Versionamento e Cabeçalhos

--- escola/serializer.py
+++ escola/serializer.py
@@ -1,24 +1,20 @@
 from rest_framework import serializers
 from escola.models import Aluno, Curso, Matricula
 
-
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
         fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
-
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
         fields = '__all__'
 
-
 class MatriculaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Matricula
         exclude = []
-
 
 class ListaMatriculasAlunoSerializer(serializers.ModelSerializer):
     curso = serializers.ReadOnlyField(source='curso.descricao')
@@ -29,9 +25,13 @@ class ListaMatriculasAlunoSerializer(serializers.ModelSerializer):
     def get_periodo(self, obj):
         return obj.get_periodo_display()
 
-
 class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
     aluno_nome = serializers.ReadOnlyField(source='aluno.nome')
     class Meta:
         model = Matricula
         fields = ['aluno_nome']
+
+class AlunoSerializerV2(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = ['id', 'nome', 'celular','rg', 'cpf', 'data_nascimento']
--- rest/urls.py
+++ rest/urls.py
@@ -5,12 +5,12 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('alunos', AlunosViewSet, basename='Alunos')
-router.register('curso', CursosViewSet, basename='Cursos')
+router.register('cursos', CursosViewSet, basename='Cursos')
 router.register('matriculas', MatriculasViewSet, basename='Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('aluno/<int:pk>/matriculas/', ListMatriculasAluno.as_view()),
-path('curso/<int:pk>/matriculas/', ListAlunosMatriculados.as_view())
+    path('alunos/<int:pk>/matriculas/', ListMatriculasAluno.as_view()),
+path('cursos/<int:pk>/matriculas/', ListAlunosMatriculados.as_view())
 ]
